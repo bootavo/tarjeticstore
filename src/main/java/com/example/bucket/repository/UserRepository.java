d9b29621cf0bc93c4b839ba881c5596f5cbbc7f1
VF Consulting <vfcons@MacBook-Pro-2.local>
2018-08-26 19:13:06 -0500

    Dependency improvements S3 and size jar optimized

--- src/main/java/com/example/bucket/repository/UserRepository.java
+++ src/main/java/com/example/bucket/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.example.bucket.repository;
 import com.example.bucket.model.Persona;
 import com.example.bucket.model.User;
 import com.google.gson.JsonObject;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.web.bind.annotation.ResponseBody;
 
